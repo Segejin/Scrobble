9f2f02617706281b7c509bcad0e18ca5ff695337
lbsilver <lb_silver@hotmail.com>
2016-02-10 20:43:41 -0800

    Add documentation

--- src/Credits.java
+++ src/Credits.java
@@ -1,3 +1,7 @@
+/**
+ * This code created by 
+ * Documented by Omar Alamoudi
+ * */
 import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
@@ -5,10 +9,19 @@ import java.awt.geom.Rectangle2D;
 public class Credits {
 	private Shape menuSquare;
 	
+    /**
+     * Create credit
+     * */
 	public Credits() {
 		menuSquare = new Rectangle2D.Float(0*.7f, 0*.7f,1200*.7f, 900*.7f);
 	}
-		
+    
+    /**
+     * Check clicked
+     * @param e mouse event
+     * @return true if menu clicked
+     * @return false if menu not clicked 
+     * */
 	public boolean clickCheck(MouseEvent e) {
 		if(menuClickCheck(e)) {
 			return true;
@@ -16,6 +29,12 @@ public class Credits {
 		return false;
 	}
 	
+    /**
+     * Click menu area
+     * @param e mouse event
+     * @return true if menu square clicked
+     * @return false if menu square not clicked 
+     * */
 	public boolean menuClickCheck(MouseEvent e) {
 		if(menuSquare.contains(e.getPoint())) {
 			return true;
