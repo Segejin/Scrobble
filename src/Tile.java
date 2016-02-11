4cdc111d69ed77770f052ae5809f5ef06e4216f4
lbsilver <lb_silver@hotmail.com>
2016-02-10 20:37:00 -0800

    Add documentation and minor naming fixes

--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,7 @@
+/**
+ * This code was created by 
+ * Documented by Omar Alamoudi
+ * */
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Shape;
@@ -14,36 +18,71 @@ public class Tile {
 	private Image greyImg;
 	private String image_folder = "rec/image/";
 
-	public Tile(char aChar, int i) {
-		this.letter = aChar;
-		this.score = i;
+    /**
+     * Initiate tile 
+     * @param letter tile letter char
+     * @param score tile score integer
+     * */
+	public Tile(char letter, int score) {
+		this.letter = letter;
+		this.score = score;
 		try {
-			this.img = ImageIO.read(new File(image_folder+aChar+".jpg"));
+			this.img = ImageIO.read(new File(image_folder+letter+".jpg"));
 			this.img = img.getScaledInstance((int)(105*.7),(int)(105*.7), Image.SCALE_DEFAULT);
-			this.greyImg = ImageIO.read(new File(image_folder+aChar+"_grey.jpg"));
+			this.greyImg = ImageIO.read(new File(image_folder+letter+"_grey.jpg"));
 			this.greyImg = img.getScaledInstance((int)(105*.7),(int)(105*.7), Image.SCALE_DEFAULT);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+    /**
+     * Get letter
+     * @return letter char 
+     * */
 	public char getLetter() {
 		return letter;
 	}
+
+    /**
+     * Set letter
+     * @param letter char
+     * */
 	public void setLetter(char letter) {
 		this.letter = letter;
 	}
+
+    /**
+     * Get score
+     * @return score int
+     * */
 	public int getScore() {
 		return score;
 	}
+
+    /**
+     * Set score
+     * @param score int
+     * */
 	public void setScore(int score) {
 		this.score = score;
 	}
+
+    /**
+     * Get Colored image
+     * @return colored image
+     * */
 	public Image getImg(){
-		return img;
-	}
+		return this.img;
+	}i
+
+    /**
+     * Get Grey image
+     * @return grey image
+     * */
 	public Image getGreyImg(){
-		return greyImg;
+		return this.greyImg;
 	}
 	/*public String getAddress() {
 		return address;
