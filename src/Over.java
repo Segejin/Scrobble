24f8fb3ece71912767883724c7d364cfddedc143
lbsilver <lb_silver@hotmail.com>
2016-02-10 22:24:49 -0800

    Add documentation

--- src/Over.java
+++ src/Over.java
@@ -9,6 +9,12 @@ public class Over {
 		menuSquare = new Rectangle2D.Float(0*.7f, 0*.7f,1200*.7f, 900*.7f);
 	}
 		
+	/**
+     * Check if mouse clicked on sellected area 
+     * @param e mouse event
+     * @return true if area clicked
+     * @return false if area not clicked
+     * */
 	public boolean clickCheck(MouseEvent e) {
 		if(menuClickCheck(e)) {
 			return true;
@@ -16,6 +22,12 @@ public class Over {
 		return false;
 	}
 	
+	/**
+     * Check if menu  botton is clicked
+     * @param e mouse event
+     * @return true if botton clicked
+     * @return false if botton not clicked
+     * */
 	public boolean menuClickCheck(MouseEvent e) {
 		if(menuSquare.contains(e.getPoint())) {
 			return true;
